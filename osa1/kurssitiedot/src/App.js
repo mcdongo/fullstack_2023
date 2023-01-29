67a9cf34e238fc8391611b197b0d6a27f6c75ab8
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-01-29 20:29:28 +0200

    Tehtävä 1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -9,9 +9,20 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
       <p>{props.part} {props.exercises}</p>
     </div>
   )
+
 }
 
 const Total = (props) => {
@@ -35,9 +46,9 @@ const App = () => {
   return (
     <div>
       <Header name={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises1={exercises3}/>
+      <Content part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2} part3={part3} 
+      exercises3={exercises3}/>
       
       <Total exercises1={exercises1} exercises2={exercises2}
       exercises3={exercises3}/>
