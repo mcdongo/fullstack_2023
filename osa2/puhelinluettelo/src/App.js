a16728d9ecb494eb49ead184edc5ddf555035cfb
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-02-06 17:42:28 +0200

    Tehtävä 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,25 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
-
-const Temp = ({text}) => {
-  return (
-    <div>debug : {text}</div>
-  )
-}
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('Nimi')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("jabadabaduu")
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
