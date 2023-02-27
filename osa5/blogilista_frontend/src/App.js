429d23b0d071e957064b275b4ac2260126b6c7b0
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-02-27 10:20:58 +0200

    Tehtävä 5.2

--- osa5/blogilista_frontend/src/App.js
+++ osa5/blogilista_frontend/src/App.js
@@ -16,19 +16,41 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username: userName, password: password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      console.log(exception)
       console.log('väärät tiedot')
     }
+  }
+
+  const logOut = () => {
+    console.log(user.username, user.token)
+    setUser(null)
+    blogService.setToken(null)
+    setUsername('')
+    setPassword('')
+    window.localStorage.removeItem('loggedBlogappUser')
   }
 
   if (user === null) {
@@ -43,7 +65,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in</p>
+      <p>
+        {user.username} logged in
+        <button onClick={logOut}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
