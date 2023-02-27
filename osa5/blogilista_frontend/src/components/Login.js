a9c9f0c2b6101449b467486b032b16fd035b229c
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-02-27 20:00:26 +0200

    Tehtävä 5.17

--- osa5/blogilista_frontend/src/components/Login.js
+++ osa5/blogilista_frontend/src/components/Login.js
@@ -6,15 +6,17 @@ const Login = ({ handleLogin, username, setUsername, password, setPassword }) =>
             username<br />
           <input type='text' value={username} name='Username'
             onChange={({ target }) => setUsername(target.value)}
+            id='username'
           />
         </div>
         <div>
             password<br />
           <input type='password' value={password} name='Password'
             onChange={({ target }) => setPassword(target.value)}
+            id='password'
           />
         </div>
-        <button type='submit'>login</button>
+        <button type='submit' id='login-button'>login</button>
       </form>
     </div>
   )
