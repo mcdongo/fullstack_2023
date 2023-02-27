b7b73b38c623145994cb201fd6145c5ee1d55c8f
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-02-27 20:45:19 +0200

    Tehtävä 5.18

--- osa5/blogilista_frontend/cypress/e2e/blog_app.cy.js
+++ osa5/blogilista_frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,35 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+
+    const user = {
+      username: 'kaarlek',
+      password: 'salasana',
+      name: 'Kaarle'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+    cy.visit('')
   })
 
   it('Login form shown', function() {
     cy.contains('log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('kaarlek')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('kaarlek logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('kaarlek')
+      cy.get('#password').type('saloasana')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
   })
 })
