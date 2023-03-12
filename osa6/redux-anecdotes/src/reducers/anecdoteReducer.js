5c8fc6cc519c70176cc08671b2885fd6a3c1e030
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-03-12 16:14:52 +0200

    Tehtävä 6.5

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -26,9 +26,10 @@ const reducer = (state = initialState, action) => {
       const id = action.payload.id
       const anecdoteToVote = state.find(a => a.id === id)
       const votedAnecdote = { ...anecdoteToVote, votes: anecdoteToVote.votes + 1 }
-      return state.map(anecdote =>
+      state = state.map(anecdote =>
         anecdote.id !== id ? anecdote : votedAnecdote
       )
+      return state.sort((a, b) => b.votes - a.votes)
 
     case 'NEW_ANECDOTE':
       const anecdote = action.payload
