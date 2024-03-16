c5cf021faa965258c9aa83e401d67a49ebf7726d
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2024-03-17 01:06:38 +0200

    Exercise 9.3

--- osa9/first/exerciseCalculator.ts
+++ osa9/first/exerciseCalculator.ts
@@ -1,11 +1,43 @@
+interface trainingArguments {
+  dailyData: number[];
+  targetHours: number;
+}
+
+const parseDailyArguments = (args: string[]): trainingArguments => {
+  if (args.length < 4) throw new Error('Not enough arguments!');
+  
+  let targetHours: number;
+
+  if (!isNaN(Number(args[2]))) {
+    targetHours = Number(args[2]);
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+
+  const dailyData: number[] = [];
+  args.slice(3).forEach((arg) => {
+    if (!isNaN(Number(arg))) {
+      dailyData.push(Number(arg));
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+
+  })
+
+  return {
+    dailyData,
+    targetHours
+  }
+}
+
 interface trainingData {
-  periodLength: number,
-  trainingDays: number,
-  success: boolean,
-  rating: number,
-  ratingDescription: string,
-  target: number,
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
 const calculateExercises = (exercises: number[], targetHours: number): trainingData => {
@@ -47,4 +79,15 @@ const calculateExercises = (exercises: number[], targetHours: number): trainingD
   }
 }
 
-console.log(calculateExercises([3,0,2,4.5,0,3,1], 2))
+try {
+  const { dailyData, targetHours } = parseDailyArguments(process.argv);
+  console.log(calculateExercises(dailyData, targetHours));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+    console.log(errorMessage);
+  }
+}
+
+//console.log(calculateExercises([3,0,2,4.5,0,3,1], 2))
