156781e9976c3d4c894488709a101dd027275d45
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2024-03-18 22:59:25 +0200

    Exercise 9.18

--- osa9/flight-diary-frontend/src/App.tsx
+++ osa9/flight-diary-frontend/src/App.tsx
@@ -1,17 +1,69 @@
 import { useState, useEffect } from "react";
 import { DiaryEntry } from "./types";
-import { getAllEntries } from "./diaryService";
+import { getAllEntries, createEntry } from "./diaryService";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState<string>('');
+  const [visibility, setVisibility] = useState<string>('');
+  const [weather, setWeather] = useState<string>('');
+  const [comment, setComment] = useState<string>('');
+  //const [errorMessage, setErrorMessage] = useState<string>('');
 
   useEffect(() => {
     getAllEntries().then(data => {
       setDiaries(data)
     })
   }, [])
+
+  const entryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    createEntry({
+      date,
+      weather,
+      visibility,
+      comment
+    }).then(data => {
+      setDiaries(diaries.concat(data));
+    })
+    setDate('');
+    setVisibility('');
+    setWeather('');
+    setComment('');
+  }
+
+
   return (
     <div>
+      <h1>Add new entry</h1>
+      {/*errorMessage*/}
+      <form onSubmit={entryCreation}>
+        date
+        <input
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+        <br />
+        visibility
+        <input
+          value={visibility}
+          onChange={(event) => setVisibility(event.target.value)}
+        />
+        <br />
+        weather
+        <input
+          value={weather}
+          onChange={(event) => setWeather(event.target.value)}
+        />
+        <br />
+        comment
+        <input
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <br />
+        <button type='submit'>add</button>
+      </form>
       <h1>Diary entries</h1>
       {diaries.map(diary => (
         <div key={diary.id}>
--- osa9/flight-diary-frontend/src/types.ts
+++ osa9/flight-diary-frontend/src/types.ts
@@ -16,6 +16,9 @@ export enum Visibility {
 export interface DiaryEntry {
   id: number;
   date: string;
-  weather: Weather;
-  visibility: Visibility;
+  weather: string;
+  visibility: string;
+  comment?: string;
 }
+
+export type NewEntry = Omit<DiaryEntry, 'id'>
